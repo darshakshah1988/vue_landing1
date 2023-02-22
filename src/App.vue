9022f40b74a7657b269292f2354f2d1c20197d1b
Darshak shah <darshakshah1988@gmail.com>
2023-02-22 11:15:02 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-blue-400 py-3 px-4 grid grid-rows-1 grid-cols-2 gap-10"><img src="/usa-logo.png" class="h-9 object-contain"><div class="relative"><a class="block max-w-xl p-2 bg-blue-500 hover:bg-blue-600 hover:cursor-pointer rounded-lg shadow-sm text-xs text-white text-center"><img src="/phone-call.png" class="h-4 inline-block"> Call Now </a></div></div>
+  <div class="my-2 text-xs text-center"><img src="/stars.png" class="w-20 inline-block mr-1"> Rated 4.9 stars based on <strong>1000+</strong> reviews. </div>
+  <div class="mx-auto max-w-3xl py-3 px-6 lg:py-8 bg-blue-400"><div class=""><!----><!----><!----><div class="text-center"><p class="text-2xl font-bold tracking-tight text-white">4 out of 5</p><img src="/qualified-users.png" class="w-1/2 mx-auto my-3"><p class="text-lg lg:text-2xl font-bold tracking-tight text-white">Qualify for Health Insurance under $10/mo.</p></div><div class="mt-3 lg:mt-5 bg-blue-200 shadow-xl py-5 px-5 rounded-lg text-center"><div><p class="text-xl lg:text-2xl text-gray-700">Are you younger than 65?</p><div class="mt-5"><div class="inline-flex rounded-md"><a href="#" class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-10 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"> Yes </a><a href="#" class="ml-5 inline-flex items-center justify-center rounded-md border border-transparent bg-white px-10 py-2 text-base font-medium text-gray-700 hover:bg-gray-50"> No </a></div></div></div><!----><!----></div><!----></div></div>
+  <section class="text-center py-5 px-5 bg-gray-100"><p class="text-gray-700 font-bold lg:text-xl">Carriers We Represent</p><img src="/carriers-bw.png" class="mt-2 lg:w-3/4 mx-auto"></section>
+  <section class="mx-5 my-5"><div class="lg:w-3/4 mx-auto flex items-start"><div class="inline-flex flex-shrink-0 rounded-full border-2 border-white"><img class="h-16 w-16 rounded-full object-cover" src="/rebecca.png" alt=""></div><div class="ml-4 text-xs"><p class="text-gray-600">"Luke is amazing! He's helped me with health insurance needs the past couple years. Not only is he extremely responsive, but he also goes above and beyond in helping you with whatever you need. Thanks Luke!"</p><p class="mt-2 font-semibold">Karelle T</p></div></div></section>
+  <section class="py-5 px-5 bg-gray-100"><p class="text-center text-gray-700 font-bold lg:text-xl">How It Works</p><div class="lg:w-3/4 mx-auto mt-2 rounded-lg shadow-lg bg-white px-5 py-2 text-xs"><p>✅ Step 1 - Qualify online at CoveredUSA.</p></div><div class="lg:w-3/4 mx-auto mt-2 rounded-lg shadow-lg bg-white px-5 py-2 text-xs"><p>✅ Step 2 - Call in to speak with a licensed agent for your area.</p></div><div class="lg:w-3/4 mx-auto mt-2 rounded-lg shadow-lg bg-white px-5 py-2 text-xs"><p>✅ Step 3 - Discover health plans that are $10 or less per month.</p></div></section>
+  <section class="mx-5 my-5"><img src="/health-check.svg" class="w-24 h-24 block mx-auto"><p class="text-center text-gray-700 font-bold lg:text-xl mt-2">About ACA Health Plans</p><p class="text-xs mt-2 lg:w-3/4 mx-auto">Nearly 16 million people signed up for this government give-back program, a record high since previous years. The big reason is that the plans are cheaper for people than they used to be.</p><p class="text-xs mt-2 lg:w-3/4 mx-auto">The federal government has pumped billions of dollars in recent years into subsidies to keep costs down for consumers. Health officials say 4 out of 5 enrollees qualify for plans that cost $10 or less per month.</p><p class="text-xs mt-2 lg:w-3/4 mx-auto">And 5 million people who are uninsured qualify for zero dollar premium plans. <strong>Are you part of the 5 million?</strong></p></section>
+  <div class="px-6 py-1"><section class="text-center my-5"><p class="mt-5 text-gray-300 text-xs">This site is not a part of the Facebook™ website or Facebook™ Inc. Additionally, this site is NOT endorsed by Facebook™ in any way. FACEBOOK™ is a trademark of FACEBOOK™, Inc.</p><p class="mt-5 text-gray-400 text-xs"><a href="/privacy-policy" class="text-underline inline-block">Privacy Policy</a> — <a href="/termsandconditions" class="text-underline inline-block">Terms And Conditions</a> — <a href="/ccpa-do-not-sell-my-information" class="text-underline inline-block">Do Not Sell My Information</a> — <a href="/ccpa-your-california-privacy-rights" class="text-underline inline-block">California Residents</a></p></section></div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
